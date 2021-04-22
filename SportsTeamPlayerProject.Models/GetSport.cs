1d0fe2e7cd6adb3ed413af40553465ec3c78aac1
JacobStull <jrstull00@gmail.com>
2021-04-22 15:13:33 -0400

    Sport Models

---
+++ SportsTeamPlayerProject.Models/GetSport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsTeamPlayerProject.Models
+{
+    public class GetSport
+    {
+        public int SportId { get; set; }
+        public string SportName { get; set; }
+        public string Team { get; set; }
+        public string Player { get; set; }
+    }
+}
